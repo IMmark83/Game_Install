14010a4d6020ef89e6ddcb20b41366552b0ce7b2
MalevichIM <malevichim@mail.ru>
2023-10-24 22:38:33 +0300

    Сhanging installation folder

--- src/Main.java
+++ src/Main.java
@@ -12,20 +12,20 @@ public class Main {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         List<File> folderList = Arrays.asList(
-                new File("C://Games//src"),
-                new File("C://Games//res"),
-                new File("C://Games//savegames"),
-                new File("C://Games//temp"),
-                new File("C://Games//src//main"),
-                new File("C://Games//src//test"),
-                new File("C://Games//res//drawables"),
-                new File("C://Games//res//vectors"),
-                new File("C://Games//res//icons")
+                new File("C://Games1//src"),
+                new File("C://Games1//res"),
+                new File("C://Games1//savegames"),
+                new File("C://Games1//temp"),
+                new File("C://Games1//src//main"),
+                new File("C://Games1//src//test"),
+                new File("C://Games1//res//drawables"),
+                new File("C://Games1//res//vectors"),
+                new File("C://Games1//res//icons")
         );
         List<File> fileList = Arrays.asList(
-                new File("C://Games//src//main//Main.java"),
-                new File("C://Games//src//main//Utils.java"),
-                new File("C://Games//temp//temp.txt")
+                new File("C://Games1//src//main//Main.java"),
+                new File("C://Games1//src//main//Utils.java"),
+                new File("C://Games1//temp//temp.txt")
         );
 
         folderList.stream().forEach(folder -> {
@@ -56,13 +56,13 @@ public class Main {
                 sb.append(ex.getMessage() + '\n');
             }
         });
-        try (FileWriter log = new FileWriter("C://Games//temp//temp.txt", false)) {
+        try (FileWriter log = new FileWriter("C://Games1//temp//temp.txt", false)) {
             log.write(sb.toString());
             log.flush();
         } catch (IOException ex) {
             sb.append(ex.getMessage() + '\n');
         }
-        try (BufferedReader br = new BufferedReader(new FileReader("C://Games//temp//temp.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C://Games1//temp//temp.txt"))) {
             String s;
             while ((s = br.readLine()) != null) System.out.println(s);
         } catch (IOException ex) {
